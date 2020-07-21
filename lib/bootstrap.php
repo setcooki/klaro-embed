3f1af39e5864b87be75520355205ba705cd5ecb9
setcookie <set@cooki.me>
2020-07-21 10:50:54 +0200

    first commit

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -2,10 +2,36 @@
 
 require_once dirname(__FILE__) . '/api.php';
 
+//currently only in wordpress environment supported
 if(function_exists('add_action'))
 {
     add_action('init', function()
     {
+        $config = null;
+        if(get_stylesheet_directory())
+        {
+            $config = get_stylesheet_directory() . '/klaroConfigEmbed.json';
+            if(is_file($config))
+            {
+                $config = json_decode(file_get_contents($config));
+            }
+        }
+        add_action('wp_enqueue_scripts', function() use ($config)
+        {
+            wp_register_style('ske', false);
+            wp_enqueue_style('ske');
+            wp_add_inline_style('ske', \Setcooki\Klaro\Embed\Embed::style());
+            wp_register_script('ske', false);
+            wp_enqueue_script('ske');
+            wp_add_inline_script('ske', \Setcooki\Klaro\Embed\Embed::script());
+            if($config && is_object($config))
+            {
+                wp_register_script('ske-config', false);
+                wp_enqueue_script('ske-config');
+                wp_add_inline_script('ske-config', 'var klaroConfigEmbed = '.json_encode($config).';');
+            }
+        }, 9999);
+
         add_filter('the_content', function($content)
         {
             $dom = new \DOMDocument();
--- src/Embed.php
+++ src/Embed.php
@@ -2,6 +2,8 @@
 
 namespace Setcooki\Klaro\Embed;
 
+use MatthiasMullie\Minify;
+
 /**
  * Class Embed
  * @package Setcooki\Klaro\Embed
@@ -20,6 +22,16 @@ class Embed
      */
     protected $attrs = [];
 
+    /**
+     * @var null
+     */
+    protected $src = null;
+
+    /**
+     * @var null
+     */
+    protected $provider = null;
+
 
     /**
      * Embed constructor.
@@ -29,8 +41,188 @@ class Embed
     public function __construct($tag, array $attrs = [])
     {
         $this->tag = $tag;
-        $this->attrs = implode(' ', array_map(function($v, $k)
-        {
+        $this->attrs = $attrs;
+        $this->src = (array_key_exists('src', $attrs)) ? $attrs['src'] : null;
+        $this->provider = $this->provider($attrs);
+    }
+
+
+    /**
+     * @return false|string
+     */
+    public static function style()
+    {
+        $svg = file_get_contents(dirname(__FILE__) . '/../assets/embed.svg');
+        $svg = preg_replace('=.*(\<svg(?:[^>].*)\>.*\<\/svg\>).*=ism', '$1', $svg);
+        $svg = str_replace("\n", '', $svg);
+        $svg = preg_replace('=\s+=i', ' ', $svg);
+        ob_start(); ?>
+        <style type="text/css">
+        .ske-embed {
+            position: relative;
+            display: block;
+            overflow: hidden;
+            max-width: 100%;
+            min-width: 320px;
+            height: auto;
+            padding-bottom: inherit;
+            background: transparent url("data:image/svg+xml;base64,<?php echo base64_encode($svg); ?>") center center/cover no-repeat;
+        }
+
+        .ske-embed-notice {
+            top: 50%;
+            left: 0;
+            position: absolute;
+            width: 100%;
+            text-align: center;
+            transform: translateY(-50%);
+            background: hsla(0, 0%, 0%, 0.8);
+            color: #fff;
+            font-size: 13px;
+            padding: 10px 5px;
+        }
+
+        .ske-embed-notice a {
+            color: #fff !important;
+            text-decoration: underline;
+        }
+
+        .ske-button {
+            display: inline-flex;
+            align-items: center;
+            justify-content: center;
+            font-family: inherit;
+            padding: 0.5em 1em;
+            text-decoration: none;
+            background: #ff0000;
+            border-radius: 20px;
+            color: #fff;
+            text-decoration: none !important;
+        }
+        </style><?php
+        return (new Minify\CSS())->add(ob_get_clean())->minify();
+    }
+
+
+    /**
+     * @return false|string
+     */
+    public static function script()
+    {
+        ob_start(); ?>
+        <script type="text/javascript">
+            var decodeBase64Raw = function (r) {
+                var o, f = {}, n = [], e = "", t = String.fromCharCode,
+                    a = [[65, 91], [97, 123], [48, 58], [43, 44], [47, 48]];
+                for (z in a) for (o = a[z][0]; o < a[z][1]; o++) n.push(t(o));
+                for (o = 0; o < 64; o++) f[n[o]] = o;
+                for (o = 0; o < r.length; o += 72) {
+                    var h, g = 0, i = 0, s = r.substring(o, o + 72);
+                    for (h = 0; h < s.length; h++) for (g = (g << 6) + f[s.charAt(h)], i += 6; i >= 8;) e += t((g >>> (i -= 8)) % 256)
+                }
+                return e
+            };
+            var decodeBase64Atob = function (r) {
+                return decodeURIComponent(atob(r).split('').map(function (c) {
+                    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+                }).join(''));
+            };
+            var decodeBase64 = function (s) {
+                s = s.trim();
+                if (typeof atob === "function") {
+                    return decodeBase64Atob(s);
+                } else if (typeof Buffer === "function") {
+                    return Buffer.from(s, "utf-8").toString("base64");
+                } else {
+                    return decodeBase64Raw(s);
+                }
+            };
+            (function ($) {
+                $(document).ready(function () {
+                    var config = null;
+                    if ('klaro' in window) {
+                        if ('embed' in klaro.getManager().config) {
+                            config = klaro.getManager().config.embed;
+                        } else if ('klaroConfigEmbed' in window) {
+                            config = window.klaroConfigEmbed;
+                        }
+                        if (config && 'provider' in config) {
+                            v
+                            var e = null;
+                            $.each(config.provider, function (i, p) {
+                                e = $('.ske-embed[data-provider="' + p.name + '"]');
+                                if (e.length) {
+                                    if ('titleText' in p) {
+                                        e.find('.ske-embed-caption').html(p.titleText);
+                                    }
+                                    if ('privacyPolicyLink' in p) {
+                                        e.find('.ske-embed-more').html('<a href="' + p.privacyPolicyLink + '" target="_blank">' + (('privacyPolicyText' in p) ? p.privacyPolicyText : 'More...') + '</a>')
+                                    }
+                                    if ('buttonText' in p) {
+                                        e.find('.ske-button').text(p.buttonText);
+                                    }
+                                }
+                            })
+                        }
+                    }
+                    $('.ske-button').on('click', function (e) {
+                        var c = $(e.currentTarget).closest('.ske-embed');
+                        if (c.length) {
+                            var s = null, h = '', p = c.data('provider');
+                            if (p) {
+                                if (config && 'provider' in config && 'klaro' in window) {
+                                    var _p = $.grep(config.provider, function (a) {
+                                        return (a.name === p);
+                                    });
+                                    _p = ('app' in _p[0]) ? _p[0]['app'] : _p[0]['name'];
+                                    klaro.getManager().updateConsent(_p, true);
+                                    klaro.getManager().saveAndApplyConsents();
+                                }
+                                $.each($('.ske-embed[data-provider="' + p + '"]'), function (i, e) {
+                                    s = $(e).next('script[type="text/template"]');
+                                    h = decodeBase64(s.html());
+                                    s.replaceWith(h);
+                                    $(e).remove();
+                                })
+                            } else {
+                                s = c.next('script[type="text/template"]');
+                                h = decodeBase64(s.html());
+                                s.replaceWith(h);
+                                c.remove();
+                            }
+                        }
+                    });
+                });
+            })(jQuery.noConflict())
+        </script><?php
+        return (new Minify\Js())->add(ob_get_clean())->minify();
+    }
+
+
+    /**
+     * @param array $attrs
+     * @return string|null
+     */
+    protected function provider(array $attrs = [])
+    {
+        if (array_key_exists('src', $attrs)) {
+            if (stripos($attrs['src'], 'vimeo') !== false) {
+                return 'vimeo';
+            } else if (stripos($attrs['src'], 'youtube') !== false) {
+                return 'youtube';
+            }
+        }
+        return null;
+    }
+
+
+    /**
+     * @param array $attrs
+     * @return string
+     */
+    protected function attrs(array $attrs = [])
+    {
+        return implode(' ', array_map(function ($v, $k) {
             return sprintf('%s="%s"', $k, $v);
         }, $attrs, array_keys($attrs)));
     }
@@ -41,16 +233,22 @@ class Embed
      */
     public function render()
     {
-        ob_start();
-        if(static::$instances === 0) { ?>
-            <style type="text/css">.test {}</style>
-        <?php } ?>
-        <div>
-
+        ob_start(); ?>
+        <div id="ske-<?php echo(static::$instances + 1); ?>" data-provider="<?php echo (string)$this->provider; ?>"
+             data-instance="<?php echo(static::$instances + 1); ?>"
+             class="ske-embed">
+            <div class="ske-embed-notice">
+                <p class="ske-embed-caption">By clicking the following link (<i
+                        style="color: #c0c0c0"><?php echo $this->src; ?></i>) you accept the data privacy statement of
+                    the corresponding external provider (<?php echo $this->provider; ?>)</p>
+                <p class="ske-embed-more"></p>
+                <p class="ske-embed-action"><a class="ske-button" href="javascript:void(0);">Click here</a></p>
+            </div>
         </div>
         <script type="text/template">
-            <?php echo base64_encode(sprintf('<%s %s></%s>', $this->tag, $this->attrs, $this->tag)); ?>
-        </script><?php
+            <?php echo base64_encode(sprintf('<%s %s></%s>', $this->tag, $this->attrs($this->attrs), $this->tag)); ?>
+        </script>
+        <?php
 
         static::$instances++;
         return ob_get_clean();
