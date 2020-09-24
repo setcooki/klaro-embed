2a55c34fe5ab68fa6c5b02be7b1b54594102b8cf
setcookie <set@cooki.me>
2020-09-24 07:29:43 +0200

    bug fix

--- src/Embed.php
+++ src/Embed.php
@@ -354,7 +354,7 @@ class Embed
     public function render()
     {
         if ($this->consented()) {
-            return sprintf('<%s %s></%s', $this->tag, $this->attrs($this->attrs), $this->tag);
+            return sprintf('<%s %s></%s>', $this->tag, $this->attrs($this->attrs), $this->tag);
         }
         $title = sprintf('By clicking the following link (<i style="color: #c0c0c0">%s></i> you accept the data privacy statement of the corresponding external provider: %s', $this->src, $this->provider);
         $button = 'Click here';
